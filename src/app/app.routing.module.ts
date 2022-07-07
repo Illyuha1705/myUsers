ab6c7a34fec4674ffaa1c9a3d9997117695d8fc0
Illia Lotvin <ilotvin@lenovo.com>
2022-07-07 10:27:50 +0300

    1.0 feat/STP-18 Add primitive not found page

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MyUsersComponent } from './components/my-users/my-users.component';
+import { PageNotFoundModule } from './modules/page-not-found-module/page-not-found.module';
+import { PageNotFoundComponent } from './modules/page-not-found-module/components/page-not-found-component/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'my-users', pathMatch: 'full' },
   { path: 'my-users', component: MyUsersComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
