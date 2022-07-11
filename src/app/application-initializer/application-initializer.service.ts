1b30666a30c4bebbc2c782941f78c3d757ce9db8
Illia Lotvin <ilotvin@lenovo.com>
2022-07-11 17:32:37 +0300

    1.0 feat/STP-22 Minor changes

--- src/app/application-initializer/application-initializer.service.ts
+++ src/app/application-initializer/application-initializer.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { execute } from 'ng-packagr';
 
 @Injectable()
 export class ApplicationInitializerService {
   constructor(private translate: TranslateService) {}
 
   public init(): Promise<void> {
-    this.translate.setDefaultLang('ua');
+    this.translate.setDefaultLang('en');
     return new Promise<void>((resolve) => {
       console.log('Initializer works');
       resolve();
--- src/app/modules/page-not-found-module/page-not-found.module.ts
+++ src/app/modules/page-not-found-module/page-not-found.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './components/page-not-found-component/page-not-found.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [PageNotFoundComponent],
+  imports: [TranslateModule],
 })
 export class PageNotFoundModule {}
